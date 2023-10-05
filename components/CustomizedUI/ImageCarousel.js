45aa0e3fd46334b5ca27e602359b8634e51c66f5
Spencer Kwon <spencerkwon92@gmail.com>
2023-10-05 23:54:00 +0900

    add image carousel

--- components/CustomizedUI/ImageCarousel.js
+++ components/CustomizedUI/ImageCarousel.js
@@ -1,66 +1,181 @@
-import React,{useState} from 'react'
+import React,{useState, useRef, useCallback} from 'react'
 import {Box, Button, HStack, Image, IconButton, Center} from '@chakra-ui/react'
 import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
+import {css} from '@emotion/react'
+import styled from '@emotion/styled'
 
 import Spacer from './Spacer'
 
+const StyledBox = styled(Box)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const buttonWrapperCss = css`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  right: 0;
+
+  .leftButton {
+    position: absolute;
+    left: 0;
+    margin-left: 10px;
+
+    background-color: rgba(240, 240, 240, 0.5);
+    :hover {
+      background-color: rgba(240, 240, 240, 0.8);
+    }
+  }
+  .rightButton {
+    position: absolute;
+    right: 0;
+    margin-right: 10px;
+
+    background-color: rgba(240, 240, 240, 0.5);
+    :hover {
+      background-color: rgba(240, 240, 240, 0.8);
+    }
+  }
+
+  .imageMarks {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin-bottom: 10px;
+  }
+`;
+
+const imageGroupCss = css`
+  display: flex;
+  flex-direction:row;
+  overflow-x: hidden;
+`
+
+const imageContainerCss = css`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 100%;
+  background-color: black;
+`
+
+const imageCss = css`
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: container;
+  margin: 0;
+
+  &.active {
+    opacity: 1;
+  }
+`;
+const indexMarkerCss = css`
+  border-radius: 50%;
+  color: rgba(240, 240, 240, 0.5);
+
+  :hover {
+    color: rgb(240, 240, 240);
+  }
+`;
+
 export default function ImageCarousel({images}) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  
-  const toPrevSlide = () =>{
+  const imageListSize = images.length;
+  const imageGroupRef = useRef(null);
 
-  }
+  const toPrevSlide = useCallback(() => {
+    if (currentImageIndex === 0) {
+      setCurrentImageIndex(imageListSize - 1);
+      imageGroupRef.current.scrollLeft = imageGroupRef.current.scrollWidth;
+    } else {
+      setCurrentImageIndex((prev) => prev - 1);
+      imageGroupRef.current.scrollLeft -= imageGroupRef.current.clientWidth;
+    }
+  },[currentImageIndex, imageListSize]);
 
-  const toNextSlide = () =>{
+  const toNextSlide = useCallback(() => {
+    if (currentImageIndex === imageListSize - 1) {
+      setCurrentImageIndex(0);
+      imageGroupRef.current.scrollLeft = 0;
+    } else {
+      setCurrentImageIndex((prev) => prev + 1);
+      imageGroupRef.current.scrollLeft += imageGroupRef.current.clientWidth;
+    }
+  },[currentImageIndex, imageListSize]);
 
-  }
+  const markerButtonHander = useCallback(
+    (index) => {
+      setCurrentImageIndex(index);
+    },
+    []
+  );
 
-  const nextSlide = () => {
-    setCurrentImageIndex((prev) => (prev + 1) % images.length);
-  };
-  
-  const prevSlide = () => {
-    setCurrentImageIndex((prev) => (prev - 1) % images.length);
-  };
-  
   return (
-    <Box>
-      <Image
-        src={`http://localhost:3065/${images[currentImageIndex].src}`}
-        alt={images[currentImageIndex].src}
-        width="100%"
-        height="auto"
-        mb={4}
-      />
-      <HStack justify="center" spacing={4}>
-        <IconButton
-          icon={<ArrowLeftIcon />}
-          aria-label="Previous"
-          onClick={toPrevSlide}
-        />
-        <IconButton
-          icon={<ArrowRightIcon />}
-          aria-label="Next"
-          onClick={toNextSlide}
-        />
-      </HStack>
-      <Spacer/>
-      <Center>
-        <HStack spacing={2}>
-          {images.map((image, index) => (
-            <Button
+    <StyledBox>
+      <div css={imageGroupCss} ref={imageGroupRef}>
+        {images.map((image, index) => (
+          <div key={index} css={imageContainerCss}>
+            <Image
               key={index}
-              size="sm"
-              variant={index === currentImageIndex ? "solid" : "outline"}
-              onClick={() => {
-                // Set currentIndex to the clicked image index
-              }}
-            >
-              {index + 1}
-            </Button>
-          ))}
+              src={`http://localhost:3065/${image.src}`}
+              alt={image.src}
+              width="100%"
+              height="auto"
+              mb={4}
+              css={imageCss}
+              className={`carousel-image ${
+                index === currentImageIndex ? "active" : ""
+              }`}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div css={buttonWrapperCss}>
+        <HStack justify="center" spacing={4}>
+          <IconButton
+            className="leftButton"
+            icon={<ArrowLeftIcon />}
+            aria-label="Previous"
+            onClick={toPrevSlide}
+            size="sm"
+            isRound
+          />
+          <IconButton
+            className="rightButton"
+            icon={<ArrowRightIcon />}
+            aria-label="Next"
+            onClick={toNextSlide}
+            size="sm"
+            isRound
+          />
         </HStack>
-      </Center>
-    </Box>
+        <Spacer />
+        <Center className="imageMarks">
+          <HStack spacing={2}>
+            {images.map((image, index) => (
+              <button
+                key={index}
+                css={indexMarkerCss}
+              >
+                {index === currentImageIndex ? "●" : "○"}
+              </button>
+            ))}
+          </HStack>
+        </Center>
+      </div>
+    </StyledBox>
   );
 }
--- components/PostImages.js
+++ components/PostImages.js
@@ -24,37 +24,12 @@ const PostImages = ({ images }) => {
       </>
     );
   }
-  if (images.length === 2) {
-    return (
-      <>
-        <div>
-          <img role="presentation" src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} width="50%" onClick={onZoom} />
-          <img role="presentation" src={`http://localhost:3065/${images[1].src}`} alt={images[1].src} width="50%" onClick={onZoom} />
-        </div>
-        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
-      </>
-    );
-  }
+
   return (
     <>
-    <ImageCarousel images={images}/>
+      <ImageCarousel images={images}/>
     </>
-    // <>
-    //   <div>
-    //     <img role="presentation" src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} width="50%" onClick={onZoom} />
-    //     <div
-    //       role="presentation"
-    //       style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
-    //       onClick={onZoom}
-    //     >
-    //       <PlusOutlined />
-    //       <br />
-    //       {images.length - 1}
-    //       개의 사진 더보기
-    //     </div>
-    //   </div>
-    //   {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
-    // </>
+
   );
 };
 
