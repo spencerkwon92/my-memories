31775b83cad6bef617222d9831ef67fb6a1ba380
Spencer Kwon <spencerkwon92@gmail.com>
2023-11-09 01:33:42 +0900

    css SSR

--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,53 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import { extractCritical } from "@emotion/server";
 
 export default class MyDocument extends Document {
+  // static async getInitialProps(ctx) {
+  //   const sheet = new ServerStyleSheet();
+  //   const originalRenderPage = ctx.renderPage;
+
+  //   try {
+  //     ctx.renderPage = () =>
+  //       originalRenderPage({
+  //         enhanceApp: (App) => (props) =>
+  //           sheet.collectStyles(<App {...props} />),
+  //       });
+
+  //     const initialProps = await Document.getInitialProps(ctx);
+  //     return {
+  //       ...initialProps,
+  //       styles: (
+  //         <>
+  //           {initialProps.styles}
+  //           {sheet.getStyleElement()}
+  //         </>
+  //       ),
+  //     };
+  //   } finally {
+  //     sheet.seal();
+  //   }
+  // }
+
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
+    const initialProps = await Document.getInitialProps(ctx);
+    const page = await originalRenderPage();
+    const styles = extractCritical(page.html);
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+    return {
+      ...initialProps,
+      ...page,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style
+            data-emotion-css={styles.ids.join(" ")}
+            dangerouslySetInnerHTML={{ __html: styles.css }}
+          />
+        </>
+      ),
+    };
   }
 
   render() {
@@ -34,7 +55,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head />
         <body>
-          <script src="https://polyfill.io/v3/polyfill.min.js?features=es2015%2Ces2016%2Ces2017%2Ces2018%2Ces2020%2Ces2019%2Ces2022%2Ces2021"/>
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=es2015%2Ces2016%2Ces2017%2Ces2018%2Ces2020%2Ces2019%2Ces2022%2Ces2021" />
           <Main />
           <NextScript />
         </body>
@@ -42,5 +63,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-
-
